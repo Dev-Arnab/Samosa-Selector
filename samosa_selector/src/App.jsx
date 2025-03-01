59403a473a1cc3141aadedcf5851c00826127b18
Dev Jyoti Ghosh Arnab <devjyotighosharnab@gmail.com>
2025-03-01 16:03:28 -0500

    Initial Environment Created

---
+++ samosa_selector/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="header">
+        <h1>Samosa Selector</h1>
+        <h2>Count: </h2>
+        <img src="Designer-removebg-preview.png"></img>
+      </div>
+    </div>
+  )
+}
+
+export default App;
